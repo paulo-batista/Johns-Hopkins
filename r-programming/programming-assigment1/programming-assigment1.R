79ae0dc25be066f422e18bd63c1854c1504c443d
Paulo Batista <paulobatista.phd@gmail.com>
2017-05-25 09:09:40 -0300

    pollutantmean algorithm updated
    pollutantmean algorithm updated

--- r-programming/programming-assigment1/programming-assigment1.R
+++ r-programming/programming-assigment1/programming-assigment1.R
@@ -1,23 +1,15 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   
-  
-  ## v <- vector()
   allFiles <- list.files(directory, pattern = "*.csv", full.names = TRUE)
   df <- do.call("rbind", lapply(c(allFiles), function(fn) 
   data.frame(filename=fn, read.csv(fn, header = TRUE, sep = ","))))
   df <- df[complete.cases(df),]
 
- ## for (i in id) {
- ##   d <- df[,pollutant]
- ##   v <- c(v, d)
- ## }  
-  
+  b = id[1]+length(id)-1
   print (id[1])
-  print (id[1]+length(id))
-  b = id[1]+length(id)
-  
-  
-  avg <- mean(df[[pollutant]][df$ID>=id[1] & df$ID<b])
+  print (b)
+ 
+  avg <- mean(df[[pollutant]][df$ID>=id[1] & df$ID<=b])
     
   return(avg)
   
