6be7aefae1704eaa9e3102bc1301339e0146ff2c
Paulo Batista <paulobatista.phd@gmail.com>
2017-05-26 16:17:20 -0300

    Reading XLSX files
    Reading XLSX files

--- getting-and-cleaning-data/basic_raw_data_handling.R
+++ getting-and-cleaning-data/basic_raw_data_handling.R
@@ -1,3 +1,12 @@
 fileUrl <- "http://download.inep.gov.br/microdados/microdados_censo_superior_2014.zip"
 download.file(fileUrl, destfile="censo_superior_2014.zip")
 
+censo <- read.table("dm_curso.csv", sep = ",", header = TRUE)
+
+
+## Reading excel files
+
+colIndex <- 2:3
+rowIndex <- 1:4
+
+cameraDataSubset <- read.xlsx("data.xlsx", sheetIndex=1, colIndex=colIndex, rowIndex=rowIndex)
